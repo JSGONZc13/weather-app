7b46b4fcb7f9d0326bd747331c7ae34be7d7fd4a
JSGONZc13 <jairo99gc@gmail.com>
2025-01-30 21:19:58 -0500

    correccion de importacion entre modules

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { FormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
   ],
   imports: [
     CommonModule,
-  ]
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  exports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
 })
-export class CoreModule { }
+export class CoreModule {}
--- src/app/weather/weather.module.ts
+++ src/app/weather/weather.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WeatherDisplayComponent } from './components/weather-display/weather-display.component';
-import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   declarations: [
@@ -12,10 +9,7 @@ import { MatButtonModule } from '@angular/material/button';
   ],
   imports: [
     CommonModule,
-    FormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule
+    CoreModule
   ],
   exports: [WeatherDisplayComponent]
 })
